f9ae5fb68e08ee32444b804cf3b05edb16f3ab0a
FrezzeLain <luka-0703@mail.ru>
2021-03-01 18:15:37 +0300

    Commit#2

--- index.js
+++ index.js
@@ -1,160 +1,217 @@
-var StartVars = {
-    MainBlock: document.getElementById('tasks'),
-    addTask: document.getElementById('add'),
-    nameTask: document.getElementById('Name'),
-    typeTask: document.getElementById('Type'),
-    timeTask: document.getElementById('Times'),
-    massTask: [],
-    massNameTask: [],
-    indexTask: 0,
-    ChangeBlock: document.getElementById('changeText'),
-    ChangeAccept: document.getElementById('accept'),
-    ChangeBack: document.getElementById('back'),
-    ChangeDes: document.getElementById('newDesc'),
-    CurrentName: undefined,
-    CurrentDes: undefined,
-    massNumber: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-};
-var Task = /** @class */ (function () {
-    function Task(Name, Type, Time) {
-        this.Description = 'Description of Task';
-        this.Name = Name;
-        this.Type = Type;
-        this.time = Time;
+    //Начальные переменные
+
+    let ClientConst = {
+        //Функциональные блоки
+        BodyBlock : document.getElementById('tasks'),
+        ButtonAddTask : document.getElementById('add'),
+        ChangeBlock : document.getElementById('changeText'),
+        ChangeAccept : document.getElementById('accept'),
+        ChangeBack : document.getElementById('back'),
+        ChangeText : document.getElementById('newDesc'),
+        //Блоки с переменными
+        BlockNameTask : document.getElementById('Name'),
+        BlockTypeTask : document.getElementById('Type'),
+        BlockTimeTask : document.getElementById('Time'),
+        //Логические данные
+        TasksMassiv : [],
+        TaskIndex : 0,
+        NumbersMassiv : ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+        CurrnetTask: ''
     }
-    return Task;
-}());
-//EventListeners
-StartVars.addTask.addEventListener('click', AddTask);
-StartVars.ChangeAccept.addEventListener('click', ChangeAccept);
-StartVars.ChangeBack.addEventListener('click', ChangeBack);
-function AddTask() {
-    var massName = StartVars.nameTask.value.trim().split('');
-    console.log(massName);
-    if (StartVars.nameTask.value.trim() === '' || StartVars.massNumber.indexOf(massName[0]) != -1) {
-        console.log('Название не подходит');
-        StartVars.nameTask.value = '';
-    }
-    else {
-        var type = StartVars.typeTask.value;
-        var time = StartVars.timeTask.value;
-        var n = StartVars.nameTask.value.trim(); //name
-        if (StartVars.massNameTask.indexOf(n) == -1) {
-            var task = new Task(n, type, time); //Add object
-            //Document.createElement
-            var div = document.createElement('div');
-            div.className = "task " + n;
-            var top_1 = document.createElement('div');
-            top_1.className = 'top';
-            var name_1 = document.createElement('h1');
-            name_1.className = 'h1Task';
-            name_1.innerText = task.Name;
-            var content = document.createElement('div');
-            content.className = 'content';
-            var describe = document.createElement('div');
-            describe.className = 'descriptionTask';
-            var p = document.createElement('p');
-            p.innerText = task.Description;
-            var about = document.createElement('div');
-            about.className = 'aboutTask flex justify-center align-center wrap';
-            var change = document.createElement('div');
-            change.className = 'changedelete flex justify-center align-center';
-            //Buttons
-            //Change
-            var buttonChange = document.createElement('button');
-            buttonChange.innerText = 'Изменить';
-            buttonChange.addEventListener('click', changeTask);
-            //Delete
-            var buttonDelete = document.createElement('button');
-            buttonDelete.innerText = 'Удалить';
-            buttonDelete.addEventListener('click', DeleteTask);
-            //Color Time
-            var h2time = document.createElement('h2');
-            h2time.className = 'h2Task';
-            if (StartVars.timeTask.value == 'Срочно') {
-                h2time.classList.add('h2Red');
-            }
-            if (StartVars.timeTask.value == 'Последний день') {
-                h2time.classList.add('h2Last');
-            }
-            if (StartVars.timeTask.value == 'В очереди') {
-                h2time.classList.add('h2Yellow');
-            }
-            if (StartVars.timeTask.value == 'Подождёт') {
-                h2time.classList.add('h2Green');
-            }
-            h2time.innerHTML = "" + task.time;
-            //Type Time
-            var h2type = document.createElement('h2');
-            h2type.className = 'h2Task';
-            h2type.innerHTML = "" + task.Type;
-            if (StartVars.typeTask.value == 'Встреча') {
-                h2type.classList.add('h2Green');
-            }
-            if (StartVars.typeTask.value == 'Код') {
-                h2type.classList.add('h2Blue');
-            }
-            if (StartVars.typeTask.value == 'Домашние дела') {
-                h2type.classList.add('h2Yellow');
-            }
-            if (StartVars.typeTask.value == 'Работа') {
-                h2type.classList.add('h2Last');
-            }
-            //node.append
-            div.append(top_1, content);
-            top_1.append(name_1);
-            content.append(describe, about, change);
-            describe.append(p);
-            about.append(h2type, h2time);
-            change.append(buttonChange, buttonDelete);
-            StartVars.MainBlock.append(div);
-            // mass.push | new index | clear
-            StartVars.massTask.push(task);
-            StartVars.massNameTask.push(n);
-            StartVars.indexTask++;
-            StartVars.nameTask.value = '';
+
+    //EventListeners
+
+    ClientConst.ButtonAddTask.addEventListener('click', NewTask)
+    ClientConst.ChangeAccept.addEventListener('click', AcceptChange)
+    ClientConst.ChangeBack.addEventListener('click', RejectChange)
+
+    //Классы
+
+    class Task{
+        config = {
+            name: '',
+            des: '',
+            type: '',
+            time: ''
         }
-    }
-}
-// Functions
-//Change Task
-function changeTask() {
-    var CurrentTask = this;
-    var DesCurrentTask = CurrentTask.parentNode.parentNode.firstChild.firstChild;
-    var NameCurrentTask = CurrentTask.parentNode.parentNode.parentNode.firstChild.firstChild;
-    StartVars.CurrentDes = DesCurrentTask;
-    StartVars.CurrentName = NameCurrentTask.textContent;
-    StartVars.massNameTask.forEach(function (element) {
-        if (element === NameCurrentTask.textContent) {
-            StartVars.ChangeBlock.style.display = "flex";
-            StartVars.ChangeDes.value = DesCurrentTask.textContent;
+
+        constructor(Name, Type, Time){
+            this.config.name = Name
+            this.config.des = 'Default Description'
+            this.config.type = Type
+            this.config.time = Time
         }
-    });
-}
-//Accept Change
-function ChangeAccept() {
-    StartVars.CurrentDes.innerText = StartVars.ChangeDes.value;
-    StartVars.ChangeBlock.style.display = "none";
-}
-//Deny Change
-function ChangeBack() {
-    StartVars.ChangeBlock.style.display = "none";
-}
-//Delete Task
-function DeleteTask() {
-    var CurrentTask = this;
-    var NameCurrentTask = CurrentTask.parentNode.parentNode.parentNode.firstChild.firstChild;
-    StartVars.massNameTask.forEach(function (elementName) {
-        if (elementName === NameCurrentTask.textContent) {
-            for (var index = 0; index < StartVars.massTask.length; index++) {
-                if (StartVars.massTask[index].Name === NameCurrentTask.textContent) {
-                    StartVars.massTask.splice(index, 1);
-                    StartVars.massNameTask.splice(StartVars.massNameTask.indexOf(NameCurrentTask.textContent), 1);
-                    StartVars.indexTask -= 1;
-                    var RemoveNode = document.querySelector('.' + NameCurrentTask.textContent);
-                    RemoveNode.remove();
-                }
+
+        renderTask(){
+            let status = IsTask(this.config.name)
+            if(status === 1){
+                console.log('Задача с таким именем уже существует.')
+            } else{
+                render(this.config)
+                ClientConst.TasksMassiv[ClientConst.TaskIndex] = this
+                ClientConst.TaskIndex++
+                ClientConst.BlockNameTask.value = ''
             }
         }
-    });
-}
+
+        deleteThisTask(){
+            let Block = ClientConst.BodyBlock.querySelector('.'+ClientConst.TasksMassiv[ClientConst.CurrnetTask].config.name)
+            Block.remove()
+        }
+
+        changeThisTask(NewDes){
+            console.log('Изменение описания:')
+            this.config.des = NewDes
+        }
+    }
+
+    //Функции
+
+        //Добавление задачи
+    
+    function NewTask(){
+        if(ClientConst.BlockNameTask.value.trim() == ''){
+            console.log('Название не может быть пустой строкой')
+        } else if(ClientConst.NumbersMassiv.indexOf(ClientConst.BlockNameTask.value.trim().split('')[0]) > -1){
+            console.log('Название не может начинаться с цифры')
+        } else{
+            let NTask = new Task(ClientConst.BlockNameTask.value.trim(), ClientConst.BlockTypeTask.value, ClientConst.BlockTimeTask.value)
+            NTask.renderTask()
+        }
+    }
+
+        //Проверка наличия Задачи
+
+    function IsTask(Name){
+        Count = ClientConst.BodyBlock.querySelectorAll(`.${Name}`).length
+        if(Count > 0){
+            return 1
+        }
+        return 0
+    }
+
+        //Генерация блоков HTML
+
+    function render(NewTask){
+         //Создание объекта Класса Task
+        let MyTask = NewTask
+        //Считывание данных объекта
+        let NameTask = MyTask.name
+        let TypeTask = MyTask.type
+        let TimeTask = MyTask.time
+        let DesTask = MyTask.des
+        //Генерация HTML - элементов
+            //Основыне блоки
+        let MainDIv = document.createElement('div')
+        MainDIv.className = (`task ${NameTask}`)
+        let TopMain = document.createElement('div')
+        TopMain.classList.add('top')
+        let MainName = document.createElement('h1')
+        MainName.classList.add('h1Task')
+            MainName.innerText = NameTask
+        let MainContent = document.createElement('div')
+        MainContent.classList.add('content')
+        let Description = document.createElement('div')
+        Description.classList.add('descriptionTask')
+            let DescriptionP = document.createElement('p')
+            DescriptionP.className = 'desP'
+            DescriptionP.innerText = DesTask
+        let MainAbout = document.createElement('div')
+        MainAbout.className = ('aboutTask flex justify-center align-center wrap')
+        let MainChange = document.createElement('div')
+        MainChange.className = ('changedelete flex justify-center align-center')
+        let Time = document.createElement('h2')
+        Time.innerText = TimeTask
+        Time.className = 'h2Task'
+        if(TimeTask == 'Срочно'){
+            Time.classList.add('h2Red')
+        }
+        if(TimeTask == 'Последний день'){
+            Time.classList.add('h2Last')
+        }
+        if(TimeTask == 'В очереди'){
+            Time.classList.add('h2Yellow')
+        }
+        if(TimeTask == 'Подождёт'){
+            Time.classList.add('h2Green')
+        }
+        let Type = document.createElement('h2')
+        Type.innerText = TypeTask
+        Type.className = 'h2Task'
+        if(TypeTask == 'Встреча'){
+            Type.classList.add('h2Green')
+        }
+        if(TypeTask == 'Код'){
+            Type.classList.add('h2Blue')
+        }
+        if(TypeTask == 'Домашние дела'){
+            Type.classList.add('h2Yellow')
+        }
+        if(TypeTask == 'Работа'){
+            Type.classList.add('h2Last')
+        }
+            //Кнопки
+        let buttonChange = document.createElement('button')
+        buttonChange.className = `htqr${ClientConst.TaskIndex}`
+        buttonChange.innerText = 'Изменить'
+        buttonChange.addEventListener('click', changeTask)
+        let buttonDelete = document.createElement('button')
+        buttonDelete.className = `htqr${ClientConst.TaskIndex}`
+        buttonDelete.innerText = 'Удалить'
+        buttonDelete.addEventListener('click', deleteTask)
+            //Node.append 
+        MainDIv.append(TopMain, MainContent)
+        TopMain.append(MainName)
+        MainContent.append(Description, MainAbout, MainChange)
+        Description.append(DescriptionP)
+        MainAbout.append(Type, Time)
+        MainChange.append(buttonChange, buttonDelete)
+        ClientConst.BodyBlock.append(MainDIv) 
+    }
+
+        //Изменение описания задачи
+
+    function changeTask(){
+        let MassName = this.className.split('')
+        for(let i = 0; i < 4; i++){
+            MassName.splice(0,1)
+        }
+        MassName = MassName.join('')
+        
+        ClientConst.ChangeBlock.style.display = `flex`
+        ClientConst.ChangeText.value = ClientConst.TasksMassiv[MassName].config.des
+        ClientConst.CurrnetTask = MassName
+    }
+
+        //Принять изменения
+
+    function AcceptChange(){
+        let NewDes = ClientConst.ChangeText.value
+        ClientConst.TasksMassiv[ClientConst.CurrnetTask].changeThisTask(NewDes)
+        let CurrentMain = document.querySelector('.'+ClientConst.TasksMassiv[ClientConst.CurrnetTask].config.name)
+        let CurrentP = CurrentMain.querySelector('.desP')
+        CurrentP.innerText = NewDes
+        ClientConst.ChangeBlock.style.display = `none`
+    }
+
+        //Отклонить изменения
+
+    function RejectChange(){
+        ClientConst.ChangeBlock.style.display = `none`
+    }
+
+        //Удаление задачи
+
+    function deleteTask(){
+        let MassName = this.className.split('')
+        for(let i = 0; i < 4; i++){
+            MassName.splice(0,1)
+        }
+        MassName = MassName.join('')
+        ClientConst.CurrnetTask = MassName
+        console.log(ClientConst.CurrnetTask)
+
+        ClientConst.TasksMassiv[MassName].deleteThisTask()
+        ClientConst.TasksMassiv[MassName] = ''
+        ClientConst.TaskIndex = ClientConst.TaskIndex - 1
+    }
